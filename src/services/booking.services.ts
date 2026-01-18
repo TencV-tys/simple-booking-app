dae335c63c8eaa6e2f31a1ce7c4bea9f9ff0bfee
Vincent <vincenttayros078@gmail.com>
2026-01-18 13:31:49 +0800

    added booking srvices

---
+++ src/services/booking.services.ts
@@ -0,0 +1,61 @@
+import prisma from "../prisma";
+import { BookingStatus } from "@prisma/client";
+export class BookingServices{
+
+           static async createBooking(userId:string, serviceType:string,date:string,time:string,notes:string){
+                           
+            const booking = await prisma.booking.create({
+                  data:{
+                      userId,
+                      serviceType,
+                      date: new Date(date),
+                      time,
+                      notes,
+                      status:'PENDING'
+                  }
+            });
+            return booking;
+
+           }
+
+
+           static async getAllBookings(){
+              const bookings = await prisma.booking.findMany();
+
+              return bookings;
+           }
+             
+           static async updateBooking(bookingId:string, newData:any){
+            
+            const booking = await prisma.booking.update({
+                where:{id:bookingId},
+                data:newData
+            });
+            
+            return booking;
+           }
+               
+
+           static async updateBookingStatus(bookingId:string, newStatus:BookingStatus){
+                 
+            const booking = await prisma.booking.update({
+                where:{
+                    id:bookingId
+                },
+               data:{ status:newStatus}
+            });
+
+            return booking;
+
+           }
+              
+           static async deleteBooking(bookingId:string){
+            const booking = prisma.booking.delete({
+                where:{
+                    id:bookingId
+                }
+            });
+
+            return booking;
+           }
+}
